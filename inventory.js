e9d11c5c8978ff828769635ac6ea6d6c0d98cf1e
Daniël Smink <smink83@gmail.com>
2015-01-29 12:00:31 +0100

    App now uses mongodb to persist stored values

--- inventory.js
+++ inventory.js
@@ -5,6 +5,7 @@
 var mongoose = require('mongoose');
 
 var itemSchema = mongoose.Schema({
+    _id: Number,
     name: String,
     description: String
 })
@@ -15,16 +16,29 @@ var items = [];
 
 var _ = require('lodash');
 
-function findOne (req) {
-    return _.find(items, {id: req.params.id});
+function getRecord (req, callback) {
+    Item.findOne({'_id':req.params.id}, function (err, item) {
+        if (err) {
+            console.error(err);
+        }
+
+        callback(err, err ? null : item);
+    });
 }
 
 exports.list = function (req, res) {
-    res.render('index', {items: items});
+    Item.find(function (err, items) {
+        if(err) {
+            console.error(err);
+        }
+        res.render('index', {items: items});
+    });
 };
 
 exports.show = function (req, res) {
-    res.render('show', findOne(req));
+    getRecord(req, function (err, item) {
+        res.render('show', item);
+    });
 };
 
 exports.new = function (req, res) {
@@ -32,34 +46,50 @@ exports.new = function (req, res) {
 };
 
 exports.create = function (req, res) {
-    var item = {
-        id: _.uniqueId(),
-        name: req.body.name,
-        description: req.body.description
-    };
-
-    items.push(item);
-    res.redirect('/');
+    if(!req.body.name) {
+        res.send('Inventory item needs at least a name');
+        res.statusCode = 400;
+    } else {
+        var item = new Item({
+            _id: _.uniqueId(),
+            name: req.body.name,
+            description: req.body.description
+        });
+        item.save(function (err, item) {
+            if(err) {
+               return console.error(err);
+            }
+            res.redirect('/');
+        });
+    }
 };
 
 exports.edit = function (req, res) {
-    res.render('edit', findOne(req));
+    getRecord(req, function (err, item) {
+        res.render('edit', item);
+    });
 };
 
 exports.update = function (req, res) {
     var id = req.params.id;
-    var index = _.findIndex(items, {id: id});
-    var item = {
-        id: id,
+
+    Item.findByIdAndUpdate(id, {
         name: req.body.name,
         description: req.body.description
-    };
+    }, function (err, result) {
+        if (err) {
+            console.error(err);
+        }
+        res.redirect('/' + id);
+    });
 
-    items[index] = item;
-    res.redirect('/' + id);
 };
 
 exports.delete = function (req, res) {
-    _.remove(items, {id: req.params.id});
-    res.json({success: true});
+    Item.remove({'_id': req.params.id}, function (err, result){
+        if (err) {
+           console.error(err);
+        }
+        res.json({success: true});
+    });
 };
