3cd80dfa133ecbd32aafa3efb127ba841210c283
Daniël Smink <smink83@gmail.com>
2015-01-29 13:59:33 +0100

    setup for using unit tests with mocha and chai

---
+++ test/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dsmink on 29/01/2015.
+ */
+
+describe('basic', function(){
+
+    it('should successfully return the base route', function() {
+
+    });
+});
---
+++ test/support/helpers.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dsmink on 29/01/2015.
+ */
+
+var chai = require('chai'),
+    chaiHttp = require('chai-http');
+
+var should = chai.should();
+
+chai.use(chaiHttp);
